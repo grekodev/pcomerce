a0912bd7ddc1375c91678bf35a998d45f9d64687
brapastor <brapastor@gmail.com>
2018-04-06 18:22:37 -0500

    agregado

--- comments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from comments.views import create_comment
-app_name = 'comments'
-
-urlpatterns = [
-    path('createcomment', create_comment, name="createcomment"),
-]
